ee838e15967c0bf5ab9c6e8224ae680edd3c11a8
Zach Smith <Lemelisk27@gmail.com>
2021-11-18 19:49:31 -0800

    added my pic

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ function Home () {
         <div className="zs-body d-flex flex-column col-12 p-5">
         <div className="col-9 mx-auto">
             <h1>About Me</h1>
-            <img src="http://placecorgi.com/260/180" className="col-4 my-4" alt="My Pic"/>
+            <img src="images/016.JPG" class="col-4 my-4 rounded" alt="My Pic"/>
             <p>I am in the process of obtaining a Web Development Certificate from the University of Washington. My years as a long distance truck driving have given me the ability to work independently, with minimal supervision, while still completing goals and deadlines. When I worked for the IRS I gave me the ability to work in a diverse environment with people from all walks of life.</p>
         </div>
     </div>
